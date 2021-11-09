d1dbc0defa9ececec7d5bf897df6ac800a804e74
Enio N. de Souza <enionsouza@gmail.com>
2021-11-09 09:53:14 -0400

    feat: Create an API endpoint that selects a random greeting from 'Greetings' table, and create a static view that for the root

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'home#index'
+
+  namespace :api, constraints: lambda { |req| req.format == :json } do
+    resources :greetings, only: [:index]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
